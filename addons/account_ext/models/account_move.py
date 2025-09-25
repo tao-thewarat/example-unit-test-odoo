685da0e2f4fc9baf40acea62f5de824df6f28c4f
tao-thewarat <ppv_tao@outlook.co.th>
2025-09-25 09:16:06 +0700

    test

--- addons/account_ext/models/account_move.py
+++ addons/account_ext/models/account_move.py
@@ -1,7 +1,32 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     reason = fields.Char(string="Reason")
+    is_ref = fields.Boolean(
+        string='Is Invoice',
+        compute='_compute_is_ref',
+    )
+
+    @api.depends('amount_untaxed')
+    def _compute_is_ref(self):
+        for rec in self:
+            if rec.amount_untaxed >= 100:
+                rec.is_ref = True
+            else:
+                rec.is_ref = False
+                
+    def _prepare_edi_vals_to_export(self):
+        return {
+            **super()._prepare_edi_vals_to_export(),
+            'reason': 'hello world',
+        }
+
+    @api.constrains('amount_untaxed')
+    def _check_amount_untaxed_limit(self):
+        for move in self:
+            if move.amount_untaxed > 500:
+                raise ValidationError("Amount untaxed cannot exceed 500!")
--- addons/account_ext/tests/test_account_move.py
+++ addons/account_ext/tests/test_account_move.py
@@ -1,15 +1,22 @@
-from odoo.tests.common import TransactionCase, Form
+from odoo.tests.common import TransactionCase
 from unittest.mock import patch
+from odoo.tests import Form
+from odoo.exceptions import ValidationError
 
 class TestAccountMove(TransactionCase):
 
     def setUp(self):
         super().setUp()
-        self.partner = self.env['res.partner'].create({
-            'name': 'Test Customer',
-        })
+        self.partner = self.env['res.partner'].create({'name': 'Test Customer'})
         self.journal = self.env['account.journal'].search([('type', '=', 'sale')], limit=1)
-        self.account = self.env['account.account'].search([], limit=1)
+        self.account = self.env['account.account'].search([('deprecated', '=', False)], limit=1)
+
+        self.invoice = self.env['account.move'].create({
+            'move_type': 'out_invoice',
+            'partner_id': self.partner.id,
+            'journal_id': self.journal.id,
+        })
+
 
     def test_create_invoice_draft(self):
         invoice = self.env['account.move'].create({
@@ -42,3 +49,34 @@ class TestAccountMove(TransactionCase):
         invoice.action_post()
         self.assertEqual(invoice.state, 'posted')
         self.assertTrue(invoice.name)
+
+    def test_compute_amount_total_form(self):
+        with Form(self.invoice) as form:
+            with form.invoice_line_ids.new() as line:
+                line.name = 'Test product'
+                line.quantity = 1
+                line.price_unit = 100
+            invoice = form.save()
+
+        self.assertTrue(invoice.is_ref)
+
+
+    def test_amount_untaxed_limit(self):
+        """Test constraint: amount_untaxed cannot exceed 500"""
+        # Happy path: amount <= 500
+        invoice = self.env['account.move'].create({
+            'move_type': 'out_invoice',
+            'partner_id': self.partner.id,
+            'journal_id': self.journal.id,
+            'invoice_line_ids': [(0, 0, {
+                'name': 'Product 1',
+                'quantity': 5,
+                'price_unit': 50,
+                'account_id': self.account.id,
+            })]
+        })
+        self.assertEqual(invoice.amount_untaxed, 250)
+
+        # Exceed limit → should raise ValidationError
+        with self.assertRaises(ValidationError):
+            self.env['account.move'].create({})
